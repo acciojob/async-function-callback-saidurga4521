e723b9039db2ca68f2050aeb220c0419bab76f48
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-03-31 12:38:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+const btnE1 = document.getElementById("btn");
+const outputE1 = document.getElementById("output");
+
+let callback = function () {
+    return url;
+};
+
+async function getData(callback) {
+    let url = callback();
+    let response = await fetch(url);
+    let data = await response.json();
+    localStorage.setItem("data", JSON.stringify(data)); // Corrected "Stringify" to "stringify"
+}
+
+getData(callback);
+
+btnE1.addEventListener('click', () => { // Corrected "addEventListiner" to "addEventListener"
+    let userdata = JSON.parse(localStorage.getItem("data"));
+    outputE1.textContent = userdata.title; // Pretty print the object
+});
 
 //your JS code here. If required.
